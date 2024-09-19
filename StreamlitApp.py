f1e41fd2f46e47dce06786925df25e2353495ed8
Anna Kotlan <2annakotlan@gmail.com>
2024-09-19 17:24:31 -0400

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -4,7 +4,6 @@ import plotly.express as px
 
 # DISPLAY **************************************************************************************************************
 st.markdown('<p style="font-weight:bold; font-size:22px;">Which Degree Best Suits You?</p>', unsafe_allow_html=True) # title
-st.write("open sidebar")
 st.sidebar.header("Select Courses") # sidebar title
 
 # DATA *****************************************************************************************************************
