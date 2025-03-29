4dd716933807e14024f3f865e6940afa6c38d0d9
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 14:01:11 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -7,7 +7,13 @@ spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
 
 def add_columns(sheet_name):
+    service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1", valueInputOption="RAW", body={"values": [['Test']]}).execute() # adding new degrees as columns
+
+
+'''
+def add_columns(sheet_name):
     spreadsheet_degrees = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', []))[0] # current spreadsheet degrees (columns), flattened
     webscraped_degrees = list(major_url_dict.keys()) # current webscraped degrees
     new_degrees = [degree for degree in webscraped_degrees if degree not in spreadsheet_degrees] # degrees in webscraped degrees not included in spreadsheet degrees
     service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1", valueInputOption="RAW", body={"values": [['Test']]}).execute() # adding new degrees as columns
+'''
