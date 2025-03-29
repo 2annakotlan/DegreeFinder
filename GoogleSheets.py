37a955803c2a26a5bd3ee5c224148747a9ff05f1
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 14:27:42 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -7,8 +7,11 @@ spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
 
 def add_column(sheet_name):
-    service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!A{len(values) + 1}:A", valueInputOption="RAW", body={"values": [['Test']]}, ).execute()
-    
+    result = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!A:A").execute()).get('values', []) # first row
+    next_row = len(values) + 1 # next row number
+    st.write(next_row)
+    service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!A{next_row}:A", valueInputOption="RAW", body={"values": [['Test']]}, ).execute() # update next column
+
 '''
 def add_columns(sheet_name):
     spreadsheet_degrees = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', []))[0] # current spreadsheet degrees (columns), flattened
