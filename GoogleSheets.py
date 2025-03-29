80c707df56b4c6318c13ecf6c3b110490b5df973
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 19:29:28 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -8,7 +8,9 @@ service = build('sheets', 'v4', credentials=Credentials.from_service_account_inf
 
 def add_column(sheet_name):
     values = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute()).get('values', []) 
+    st.write(values)
     next_column = len(values[0])
+    st.write(next_column)
     column_letter = chr(64 + next_column) if next_column <= 26 else chr(64 + (next_column - 1) // 26) + chr(65 + (next_column - 1) % 26)
     st.write(column_letter)
     #service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!A{next_row}:A", valueInputOption="RAW", body={"values": [['Test']]}, ).execute() # update next column
