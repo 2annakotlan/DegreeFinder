950862659bb94b0f188ac2af94bab1e58eb46595
Anna Kotlan <2annakotlan@gmail.com>
2025-03-28 23:22:13 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -14,10 +14,12 @@ service = get_sheets_service()
 def append_row_by_headers(data, sheet_name="Sheet1"):
     column_headers = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute().get('values', [])[0]
     missing_columns = [header for header in data.keys() if header not in column_headers]
-    if missing_columns: 
-        st.write("Missing columns:", missing_columns)
-        return
+    
+    if missing_columns:
+        column_headers.extend(missing_columns)
+        column_headers.sort()  
+        service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1", valueInputOption="RAW", body={"values": [column_headers]}).execute()
+
     values = [data.get(header, "") for header in column_headers]
     service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=sheet_name, valueInputOption="RAW", body={"values": [values]}).execute()
-
 
