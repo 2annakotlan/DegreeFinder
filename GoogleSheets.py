7e9c29e5b2d769d9d3480333fa77c5167c842d56
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 13:28:26 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -5,9 +5,8 @@ from google.oauth2.service_account import Credentials
 spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
 
-def get_columns(sheet_name):
-    values = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', [])
-    return (values)
-
-
-    
+def add_columns(sheet_name):
+    spreadsheet_degrees = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', []))[0] # current spreadsheet degrees (columns), flattened
+    webscraped_degrees = list(major_url_dict.keys()) # current webscraped degrees
+    new_degrees = [degree for degree in webscraped_degrees if degree not in spreadsheet_degrees] # degrees in webscraped degrees not included in spreadsheet degrees
+    sheet.values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1", valueInputOption="RAW", body={"values": [new_degrees]}).execute() # adding new degrees as columns
