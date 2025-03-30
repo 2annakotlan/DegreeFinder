858ffe93eb15fe1b2096737fdc8d7b985d22df96
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 21:08:47 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -7,13 +7,12 @@ spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
 
 def add_column(sheet_name): 
-    values = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute()).get('values', []) 
-    next_column_number = len(values[0]) + 1
-    next_column_letter = chr(64 + next_column_number) if next_column_number <= 26 else chr(64 + (next_column_number - 1) // 26) + chr(65 + (next_column_number - 1) % 26)
-
-    requests = [{"updateSheetProperties": {"properties": {"sheetId": 0}, "fields": "gridProperties.columnCount", "gridProperties": {"columnCount": next_column_number}}}]
-    st.write(requests)
-
+    #values = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute()).get('values', []) 
+    #next_column_number = len(values[0]) + 1
+    #next_column_letter = chr(64 + next_column_number) if next_column_number <= 26 else chr(64 + (next_column_number - 1) // 26) + chr(65 + (next_column_number - 1) % 26)
+    #requests = [{"updateSheetProperties": {"properties": {"sheetId": 0}, "fields": "gridProperties.columnCount", "gridProperties": {"columnCount": next_column_number}}}]
+    
+    requests = [{"updateSheetProperties": {"properties": {"sheetId": 0}, "fields": "gridProperties.columnCount", "gridProperties": {"columnCount": 37}}}]
     service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={"requests": requests}).execute()
     #service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!{next_column_letter}1:{next_column_letter}", valueInputOption="RAW", body={"values": [['New Column']]}).execute()
 
