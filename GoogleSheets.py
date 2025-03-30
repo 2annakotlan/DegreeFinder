df071b640ce0a704e94700b10671e2d81444b803
Anna Kotlan <2annakotlan@gmail.com>
2025-03-30 01:51:52 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from datetime import datetime
 from googleapiclient.discovery import build 
 from google.oauth2.service_account import Credentials 
-from DegreeLinksDict import major_url_dict, minor_url_dict   
+from DegreeLinksDict import major_url_dict, minor_url_dict  
 
 spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
@@ -21,7 +22,9 @@ def update_columns(sheet_name, sheet_id):
         service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=f"{sheet_name}!{letter_column_needed}1", valueInputOption="RAW", body={"values": [new_degrees]}).execute() # fill in new column
 
 # UPDATE SPREADSHEET WITH RESUlts **************************************************************************************
-def append_data(data, sheet_name):
+def append_data(data, id, sheet_name):
     spreadsheet_degrees = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', []))[0] # spreadsheet columns
-    values = [data.get(degree, 0) for degree in spreadsheet_degrees] # create row to append, matching dictionary keys to headers
+    values = [data.get(degree, 0) for degree in spreadsheet_degrees] # create row to append - in correct position, matching dictionary keys to headers
+    values[spreadsheet_degrees.index('Student ID')] = id  # put id in correct position
+    values[spreadsheet_degrees.index('timestamp')] = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # set current timestamp in correct position
     service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=f"{sheet_name}", valueInputOption="RAW", body={"values": [values]}).execute() # fill in row
