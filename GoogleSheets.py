0a053df7bc308f1df61e1b638a97fcd3c942afb1
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 20:55:39 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -10,6 +10,9 @@ def add_column(sheet_name):
     values = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute()).get('values', []) 
     next_column_number = len(values[0]) + 1
     next_column_letter = chr(64 + next_column_number) if next_column_number <= 26 else chr(64 + (next_column_number - 1) // 26) + chr(65 + (next_column_number - 1) % 26)
+
+    requests = [{"updateSheetProperties": {"properties": {"sheetId": sheet_name}, "fields": "gridProperties.columnCount", "gridProperties": {"columnCount": next_column_number}}}]
+    service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={"requests": requests}).execute()
     service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!{next_column_letter}1:{next_column_letter}", valueInputOption="RAW", body={"values": [['New Column']]}).execute()
 
 '''
