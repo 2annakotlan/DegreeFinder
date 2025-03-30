321da949f4faef80a5090d87e80c08a246ec640d
Anna Kotlan <2annakotlan@gmail.com>
2025-03-29 21:23:23 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -6,15 +6,12 @@ from DegreeLinksDict import major_url_dict, minor_url_dict
 spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
 
-def add_column(sheet_name): 
-    values = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute()).get('values', []) 
-    next_column_number = len(values[0]) + 1
-    #next_column_letter = chr(64 + next_column_number) if next_column_number <= 26 else chr(64 + (next_column_number - 1) // 26) + chr(65 + (next_column_number - 1) % 26)
-    #requests = [{"updateSheetProperties": {"properties": {"sheetId": 0}, "fields": "gridProperties.columnCount", "gridProperties": {"columnCount": next_column_number}}}]
-    requests = [{"updateSheetProperties": {"properties": {"sheetId": 0, "gridProperties": {"columnCount": next_column_number}}, "fields": "gridProperties.columnCount"}}]
-    service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={"requests": requests}).execute()
-    #service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!{next_column_letter}1:{next_column_letter}", valueInputOption="RAW", body={"values": [['New Column']]}).execute()
-
+def add_column(sheet_name, sheet_id): 
+    values = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f"{sheet_name}!1:1").execute()).get('values', []) # read first row
+    next_column_number = len(values[0]) + 1 # length of first row + 1
+    service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={"requests": [{"updateSheetProperties": {"properties": {"sheetId": sheet_id, "gridProperties": {"columnCount": next_column_number}}, "fields": "gridProperties.columnCount"}}]}).execute() # add a empty column
+    service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=f"{sheet_name}!AK1", valueInputOption="RAW", body={"values": [['Test']]}).execute() # fill in new column
+    
 '''
 def add_columns(sheet_name):
     spreadsheet_degrees = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', []))[0] # current spreadsheet degrees (columns), flattened
