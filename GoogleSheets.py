822b9dbcd7073ee407dac73bd3ab5842c6931acd
Anna Kotlan <2annakotlan@gmail.com>
2025-04-02 17:01:25 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -2,7 +2,6 @@ import streamlit as st
 from datetime import datetime
 from googleapiclient.discovery import build 
 from google.oauth2.service_account import Credentials 
-#from DegreeLinksDict import major_url_dict, minor_url_dict  
 
 spreadsheetId = '16xVJWtgcHnHUFU9kbQ8N_QHb4mXX57KiN3WyDooApTY'
 service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))
