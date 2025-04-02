45a58f344c6fee7d929e55761869db1def4b1604
Anna Kotlan <2annakotlan@gmail.com>
2025-04-02 17:05:57 -0400

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -38,13 +38,12 @@ def display_login_page():
         major_1, major_2 = (major + [""] * 2)[:2] # assigning major, defaulting to empty string if nothing selected
         minor_1, minor_2 = (minor + [""] * 2)[:2] # assigning minor, defaulting to empty string if nothing selected
         submitted = st.form_submit_button("Next") # submit
-
-    st.markdown('<p style="font-weight:bold;">Designed by Anna Kotlan, Class of 2025</p>', unsafe_allow_html=True)
     
     if submitted and not id:
         st.error("Student ID Required") # error message 
 
     if submitted and id:
+        st.session_state["id"] = id # save in state session
         # append_student_data(id = id, major_1 = major_1, major_2 = major_2, minor_1 = minor_1, minor_2 = minor_2) # google sheets   
         st.session_state.page = 'display_analytics_page'
         st.rerun()
@@ -167,6 +166,7 @@ def display_analytics_page():
         #update_prediction_columns(sheet_name = "MinorPredictions", sheet_id = 375147427) 
         
         # update spreadsheet with results
+        id = st.session_state.get("id") # retrieve stored id
         append_prediction_data(data = major_degree_matches_dict, id = id, sheet_name = "MajorPredictions") 
         append_prediction_data(data = minor_degree_matches_dict, id = id, sheet_name = "MinorPredictions") 
             
