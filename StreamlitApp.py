23d8b7f72532bc6a0adbfe556d9420cda01e2093
Anna Kotlan <2annakotlan@gmail.com>
2025-04-01 22:19:49 -0400

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
-import plotly.express as px
+import plotly.express as px  
 import streamlit as st
 import pandas as pd 
-import re   
+import re    
 
 # DATA *****************************************************************************************************************
 # degree requirements dictionary
