5f05aa03706c55386245576751bbe6fb9fca68f8
Anna Kotlan <2annakotlan@gmail.com>
2025-04-02 22:39:22 -0400

    Update GoogleSheets.py

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -22,11 +22,18 @@ def update_prediction_columns(sheet_name, sheet_id):
 
 # UPDATE SPREADSHEET WITH PREDICTION RESUltS ***************************************************************************
 def append_prediction_data(data, id, sheet_name):
+    id_column = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!A2:A').execute().get('values', [])
+    row_number = next((row_number for row_number, row in enumerate(id_column, start=2) if row and row[0] == id), None) # find row number where id already exists
+
     spreadsheet_degrees = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=f'{sheet_name}!1:1').execute().get('values', []))[0] # spreadsheet columns
     values = [data.get(degree, 0) for degree in spreadsheet_degrees] # create row to append - in correct position, matching dictionary keys to headers
     values[spreadsheet_degrees.index('Student ID')] = id  # put id in correct position
     values[spreadsheet_degrees.index('Timestamp')] = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # set current timestamp in correct position
-    service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=f"{sheet_name}", valueInputOption="RAW", body={"values": [values]}).execute() # fill in row
+
+    if row_number == None: 
+        service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range=f"{sheet_name}", valueInputOption="RAW", body={"values": [values]}).execute() # append data in next row
+    else: 
+        service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"{sheet_name}!{row_number}:{row_number}", valueInputOption="RAW", body={"values": [values]}).execute() # replace data in specified row
 
 # UPDATE SPREADSHEET WITH STUDENT DATA *********************************************************************************
 def append_student_data(id, major_1, major_2, minor_1, minor_2):
@@ -36,12 +43,12 @@ def append_student_data(id, major_1, major_2, minor_1, minor_2):
     spreadsheet_columns = (service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range="StudentInfo!1:1").execute().get('values', []))[0] # spreadsheet columns
     values = [''] * len(spreadsheet_columns) # placeholder to be replaced with data
     values[spreadsheet_columns.index('Student ID')] = id  # put id in correct position
-    values[spreadsheet_columns.index('Declared Major 1')] = major_1  # put major 1 in correct position
-    values[spreadsheet_columns.index('Declared Major 2')] = major_2  # put major 2 in correct position
-    values[spreadsheet_columns.index('Declared Minor 1')] = minor_1  # put minor 1 in correct position
-    values[spreadsheet_columns.index('Declared Minor 2')] = minor_2  # put minor 2 in correct position
+    values[spreadsheet_columns.index('Major 1')] = major_1  # put major 1 in correct position
+    values[spreadsheet_columns.index('Major 2')] = major_2  # put major 2 in correct position
+    values[spreadsheet_columns.index('Minor 1')] = minor_1  # put minor 1 in correct position
+    values[spreadsheet_columns.index('Minor 2')] = minor_2  # put minor 2 in correct position
 
-    if row_number == None: # if this is the first time a student is logging in...
+    if row_number == None: 
         service.spreadsheets().values().append(spreadsheetId=spreadsheetId, range="StudentInfo", valueInputOption="RAW", body={"values": [values]}).execute() # append data in next row
-    else: # if a student has already logged in...
+    else: 
         service.spreadsheets().values().update(spreadsheetId=spreadsheetId, range=f"StudentInfo!{row_number}:{row_number}", valueInputOption="RAW", body={"values": [values]}).execute() # replace data in specified row
