d72bd474fa434918b227d760f51c777df9cb1e55
Anna Kotlan <2annakotlan@gmail.com>
2025-04-05 19:02:20 -0400

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -39,12 +39,12 @@ def display_email_page():
     if user_email:
         verification_code = send_verification_code(user_email) # send verification code
     
-    user_code = st.text_input("Verification Code: ") # verification code
+        user_code = st.text_input("Verification Code: ") # verification code
 
-    if user_code == str(verification_code):
-        st.success("Email verified successfully!")
-    else:
-        st.error("Invalid verification code. Please try again.")
+        if user_code == str(verification_code):
+            st.success("Email verified successfully!")
+        else:
+            st.error("Invalid verification code. Please try again.")
 
 # DISPLAY LOGIN PAGE ***************************************************************************************************
 def display_login_page():    
