c8e914cd5108800e3e85930a4df70424d2419ac9
Anna Kotlan <2annakotlan@gmail.com>
2025-04-05 19:11:11 -0400

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -38,6 +38,7 @@ def display_email_page():
     
     if user_email:
         verification_code = send_verification_code(user_email) # send verification code
+        st.write(verification_code)
     
         user_code = st.text_input("Verification Code: ") # verification code
         
