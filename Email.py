11decd3dfe98542aa99782a4e7325e4c33fb75a3
Anna Kotlan <2annakotlan@gmail.com>
2025-04-06 00:32:39 -0400

    Update Email.py

--- Email.py
+++ Email.py
@@ -29,10 +29,8 @@ def send_verification_code(user_email):
         # send the email
         server.sendmail(sender_email, user_email, msg.as_string())
         server.quit()
-        return verification_code
 
     except Exception as e:
         st.error(f"Failed to send verification code: {e}")
-        return None
 
 
