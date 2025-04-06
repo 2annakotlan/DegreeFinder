05769fd516e01b22e54454673d5b05d1bc8d8031
Anna Kotlan <2annakotlan@gmail.com>
2025-04-06 00:30:54 -0400

    Update Email.py

--- Email.py
+++ Email.py
@@ -1,38 +1,38 @@
+import streamlit as st
 import smtplib
 import random
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import streamlit as st
 
 def send_verification_code(user_email):
     sender_email = "noreply.socialnetworkapp@gmail.com"
     sender_password = "mmfg chsp hbhz hhav"
-
+    
+    # generate random 4-digit verification code
+    verification_code = random.randint(1000, 9999) 
+    
     try:
-        # generate random 4-digit verification code
-        verification_code = random.randint(1000, 9999)
-
         # set up SMTP server
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(sender_email, sender_password)
-
+    
         # construct email message
-        subject = "Degree Finder Verification Code"
+        subject = "Your Verification Code"
         body = f"Your verification code is: {verification_code}"
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = user_email
+        msg = MIMEMultipart()  
+        msg['From'] = 'Social Network App <noreply.socialnetworkapp@gmail.com>'
+        msg['To'] = user_email.strip()
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'plain'))
-
+    
         # send the email
         server.sendmail(sender_email, user_email, msg.as_string())
         server.quit()
         return verification_code
-        
+
     except Exception as e:
         st.error(f"Failed to send verification code: {e}")
         return None
-    
-    
+
+
